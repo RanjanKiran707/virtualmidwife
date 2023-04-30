573063b06e4dee3879c0572ed8757afddc6e6c3c
RanjanKiran707 <77231463+RanjanKiran707@users.noreply.github.com>
2023-04-30 20:28:42 +0530

    c

--- index.js
+++ index.js
@@ -13,51 +13,51 @@ const orders = require('./routes/orders');
 const userinfo = require('./routes/userinfo');
 const cookieParser = require('cookie-parser');
 const diet = require('./routes/diet');
-const {ocrSpace}=require('ocr-space-api-wrapper');
+const { ocrSpace } = require('ocr-space-api-wrapper');
 const fs = require('fs')
 const path = require('path')
-const requestmod= require("request");
+const requestmod = require("request");
 const multer = require('multer');
 const { request } = require('http');
 const upload = multer({})
 var number = 4
 
-const port = process.env.PORT||4000
- mongoose.connect('mongodb+srv://Pratap11:QY6we3pEfj5uvlDe@cluster0.oejn7.mongodb.net/?retryWrites=true&w=majority',{
-    useNewUrlParser:true 
-}).then(()=>{
+const port = process.env.PORT || 4000
+mongoose.connect('mongodb+srv://Pratap11:QY6we3pEfj5uvlDe@cluster0.oejn7.mongodb.net/?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+}).then(() => {
     console.log("Mongo connection successful!")
-}).catch(()=>{
+}).catch(() => {
     console.log("Error in connecting to the database! and going")
 })
 console.log("hey")
 
 //check whether the date is past or future
-const checkDate = (date)=>{ 
+const checkDate = (date) => {
     var today = new Date();
     var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
+    var mm = today.getMonth() + 1; //January is 0!
     var yyyy = today.getFullYear();
-    if(dd<10) { 
-        dd='0'+dd
+    if (dd < 10) {
+        dd = '0' + dd
     }
-    if(mm<10) {
-        mm='0'+mm
+    if (mm < 10) {
+        mm = '0' + mm
     }
-    today = yyyy+'-'+mm+'-'+dd;
-    if(today>date){
+    today = yyyy + '-' + mm + '-' + dd;
+    if (today > date) {
         return true
     }
-    else{
+    else {
         return false
     }
 
-    }
+}
 
 //get user info from the database
-const getUserInfo = async (req,res,next)=>{ 
+const getUserInfo = async (req, res, next) => {
     const user = await User.findById(req.user._id)
-    if(!user){
+    if (!user) {
         return res.status(404).send('User not found')
     }
     req.user = user
@@ -68,116 +68,123 @@ const getUserInfo = async (req,res,next)=>{
 
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.get("/viewusers",async (req,res)=>{
-    const user = User.findOne({name:"Pratap Simha"},(err,userObj)=>{
+app.use(express.urlencoded({ extended: true }))
+app.get("/viewusers", async (req, res) => {
+    const user = User.findOne({ name: "Pratap Simha" }, (err, userObj) => {
 
-        if(err){
-            console.log("The error is-"+err)
+        if (err) {
+            console.log("The error is-" + err)
         }
-        else{
+        else {
             console.log(userObj)
         }
-        
+
     });
-    
+
 })
 
 
 
-const handleUpload = async (req,res,next)=>{
-    
-    number +=1;
+const handleUpload = async (req, res, next) => {
+
+    number += 1;
     const tmp = await fs.writeFileSync(
         path.join(__dirname, `./reports/test.pdf`),
         req.files[0].buffer)
-        try{
-            var res1 = await ocrSpace(`./reports/test.pdf`, {apiKey:'K87663152988957'})
-            res1 = res1.ParsedResults[0].ParsedText;
-            var newstring = res1.split(" ").join("");
-            newspacestring=newstring.replace(/(\r\n|\n|\r)/gm, "");
-            console.log(newstring)
+    try {
+        var res1 = await ocrSpace(`./reports/test.pdf`, { apiKey: 'K87663152988957' })
+        res1 = res1.ParsedResults[0].ParsedText;
+        var newstring = res1.split(" ").join("");
+        newspacestring = newstring.replace(/(\r\n|\n|\r)/gm, "");
+        console.log(newstring)
 
-            // https.get(`https://dry-coast-77112.herokuapp.com/?info=${newstring}`,(resp,err)=>{
-            //     if(err){
-            //         console.log(err)
-            //     }
-            //     else{
-            //         console.log(resp)
-            //     }
-                
-            // })
-            requestmod(`https://dry-coast-77112.herokuapp.com/?info=${newspacestring}`,(err,rest,body)=>{
-                if(err){
-                    console.log(err)
-                }
-                else{
-                    var array = JSON.parse(body);
-                    console.log(array[1]);
-                    if(array[array.length-1]==1){
-                        res.send([array,"Normal"])
-                    }
-                    else if(array[array.length-1]==2){
-                        res.send([array,"Suspect"])
-                    }
-                    else{
-                        res.send([array,"Pathological"])
-                    }
-                }
-            })
+        // https.get(`https://dry-coast-77112.herokuapp.com/?info=${newstring}`,(resp,err)=>{
+        //     if(err){
+        //         console.log(err)
+        //     }
+        //     else{
+        //         console.log(resp)
+        //     }
 
-        }
-        catch{
-            console.log("Error");
-        }
+        // })
+        requestmod(`https://dry-coast-77112.herokuapp.com/?info=${newspacestring}`, (err, rest, body) => {
+            if (err) {
+                console.log(err)
+            }
+            else {
+                var array = JSON.parse(body);
+                console.log(array[1]);
+                if (array[array.length - 1] == 1) {
+                    res.send([array, "Normal"])
+                }
+                else if (array[array.length - 1] == 2) {
+                    res.send([array, "Suspect"])
+                }
+                else {
+                    res.send([array, "Pathological"])
+                }
+            }
+        })
+
+    }
+    catch {
+        console.log("Error");
+    }
     // const upl = fs.writeFileSync(path.join(__dirname, '/test.pdf'), file);
     // res.send("file has been uploaded");
 }
 
+var cors = require('cors');
 
+// use it before all route definitions
+app.use(cors({ origin: 'https://ranjankiran707.github.io/' }));
 app.use("/auth", auth);
-app.use("/details",userinfo);
-app.use("/inventory",inventory);
-app.use("/exercises",exercise);
-app.use("/order",orders)
-app.use("/forum",forum);
-app.use("/diet",diet);
+app.use("/details", userinfo);
+app.use("/inventory", inventory);
+app.use("/exercises", exercise);
+app.use("/order", orders)
+app.use("/forum", forum);
+app.use("/diet", diet);
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send(`You have reached the server for Symbiot VAR TEMP team.`)
 })
 console.log("Test")
-app.post("/adduser",async (req,res)=>{
+app.post("/adduser", async (req, res) => {
     const user = new User({
-         name:"Pratap Simha",
-         age:21
+        name: "Pratap Simha",
+        age: 21
     })
-    try{
-         const successUser = await user.save();
-         res.send("Success in adding the user!")  
+    try {
+        const successUser = await user.save();
+        res.send("Success in adding the user!")
     }
-    catch(err){
+    catch (err) {
         console.log("Error in adding new users")
     }
-})   
+})
 
 // app.post("/getocr",async (req,res)=>{
-    // try{
-    //     const res1 = await ocrSpace('./sample.pdf', {apiKey:'K87663152988957'})
-    //     console.log(res1)
-    // }
-    // catch{
-    //     console.log("Error");
-    // }
+// try{
+//     const res1 = await ocrSpace('./sample.pdf', {apiKey:'K87663152988957'})
+//     console.log(res1)
+// }
+// catch{
+//     console.log("Error");
+// }
 // })
 
-app.post("/fileupload",upload.any(),handleUpload)
+app.post("/fileupload", upload.any(), handleUpload)
 
-app.listen(port,(err)=>{
-    if(err){
+var server = app.listen(port, "127.0.0.1", (err) => {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('running at http://' + host + ':' + port)
+    if (err) {
         console.log(err)
     }
-    else{
+    else {
+        console.log(port)
         console.log("Server has started")
     }
 
